02151943d123a68f8c7a04040ea0d22140e6e9f2
zhelyazko777 <zhelyzko@abv.bg>
2021-10-22 23:58:34 +0300

    Small changes

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -246,14 +246,17 @@ export default {
     filters: {
         replaceLinkId (link, row) {
             const regex = `${encodeURI('[')}.+\\${encodeURI(']')}`;
-            const itemsToReplace = link.match(RegExp(regex))[0].split('/');
-            for (const item of itemsToReplace) {
-                const trimmedItem = item.replace(encodeURI('['), '').replace(encodeURI(']'), '');
-                if (trimmedItem === 'itemId') {
-                    link = link.replace(encodeURI('[itemId]'), row.id);
-                } else {
-                    link = link.replace(encodeURI(`[${trimmedItem}]`), row[trimmedItem]);
-                }
+            const match = link.match(RegExp(regex));
+            if (match && match.length > 0) {
+              const itemsToReplace = match[0].split('/');
+              for (const item of itemsToReplace) {
+                  const trimmedItem = item.replace(encodeURI('['), '').replace(encodeURI(']'), '');
+                  if (trimmedItem === 'itemId') {
+                      link = link.replace(encodeURI('[itemId]'), row.id);
+                  } else {
+                      link = link.replace(encodeURI(`[${trimmedItem}]`), row[trimmedItem]);
+                  }
+              }
             }
 
             return link;
